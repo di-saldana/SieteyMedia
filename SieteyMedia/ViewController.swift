247c28cb1853c05c9eb464ec8a22d7c9e6d7434e
di-saldana <dsl42@alu.ua.es>
2023-10-28 14:25:49 +0200

    Initial Commit

---
+++ SieteyMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SieteyMedia
+//
+//  Created by Dianelys Saldaña on 10/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
